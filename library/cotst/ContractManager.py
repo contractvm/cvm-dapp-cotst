d8a2fcb2d44716f5ca9f9227b166938612d1511b
Davide Gessa <gessadavide@gmail.com>
2015-10-11 15:17:53 +0200

    cotst minors

--- library/cotst/ContractManager.py
+++ library/cotst/ContractManager.py
@@ -10,7 +10,7 @@ import logging
 import time
 from threading import Thread, Lock
 from libcontractvm import Wallet, ConsensusManager, Log, DappManager
-import ContractException
+from . import ContractException
 
 logger = logging.getLogger('libcontractvm')
 
